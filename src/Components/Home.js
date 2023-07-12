e482ac062aaf4273a3cc2be40f107fbf6b18d4a1
Sattyam Samania <67833888+SattyamSamania@users.noreply.github.com>
2023-07-12 12:03:32 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,45 @@
-import React, { Component } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import React, { Component, useState } from 'react'
 import { Text, View, StyleSheet, Button } from 'react-native';
 
 
 
-export default function Home(navigation){
+export default function Home(){
+    const navigation = useNavigation();
+
+    const [counter, setCounter] = useState("");
+
+
+    const  incrementCounter = () =>{
+        setCounter(counter + 1);
+    }
+
+    const resetCounter =()=>{
+        setCounter(0);
+    }
+
+    const decrementCounter =() =>{
+        setCounter(counter - 1);
+    }
     return(
+        <>
         <View style={styles.mainContainer}> 
+
+        
             <Text style={styles.mainHeader}>Welcome to  Home Page </Text>
             <Button title='Go Back' 
             
             // onPress={navigation.navigate{Login}}
             />
+
+            <Text style={styles.text}>Counter: {counter}</Text>
+            <Button title='increment' onPress={incrementCounter} />
+            <Button title='reset' onPress={resetCounter} />
+            <Button title='decrement' onPress={decrementCounter} />
+
         </View>
+        
+      </>
     )
 }
 
@@ -28,6 +56,10 @@ const styles = StyleSheet.create({
     mainHeader: {
         fontSize: 30,
         color: '#45621'
-    }
+    },
+     text: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
 
 }) 
