26234c3f31dd40a60282ff2f0f71b6663b717fd8
Sattyam Samania <67833888+SattyamSamania@users.noreply.github.com>
2023-07-20 10:05:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image,Text, Button,  View } from 'react-native';
-import Login from './src/Components/Login';
-import Home from './src/Components/Home'
+import { StyleSheet, Image,Text, Button, TouchableOpacity,  View } from 'react-native';
+import Login from './screens/Login';
+import HomeScreen from './screens/HomeScreen'
+import MovieScreen from './screens/MovieScreen'
+import { useNavigation } from '@react-navigation/native';
+// import BackButton from './components/BackButton';
+
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HeaderBackButton } from 'react-navigation-stack';
+// import ProfileThumb from './components/ProfileThumb';
+// import InfoCard from './components/InfoCard';
+// import ProgressBar from './components/ProgressBar';
 
+const Stack = createNativeStackNavigator();
+const handleLogout= ({navigation})=> {
+  // console.log("navigationnnnnr", navigation);
+  navigation.navigate('Login')
+}
 
-export default function App() {
-  const Stack = createNativeStackNavigator();
-
+export default function App( ) {
   return (
 
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name='Login' component={Login}/>
-      <Stack.Screen name='Home' component={Home} />
+      <Stack.Screen name='Home' component={HomeScreen}  
+      
+      options={(navigation) => ({ 
+        headerBackVisible:false,
+        headerTitle: 'Dashboard',
+        headerRight : () => {
+          return(
+
+            // <Text>Logout</Text>
+            
+              <TouchableOpacity
+               style={{ backgroundColor: '#29434e' , padding: 10}}
+               onPress={()=>handleLogout(navigation)}
+             >
+               <Text style={{ marginVertical:1, borderRadius: 25, color: 'rgba(255,255,255,0.8)', fontSize: 20,}}> Logout </Text>
+             </TouchableOpacity>   
+            
+          )
+        } 
+       
+      })}
+     
+      />
+      <Stack.Screen name='Movie' component={MovieScreen} 
+      
+      
+      options={(navigation) => ({
+
+        headerTitle: 'MovieDetails',
+        headerRight: () => {
+          return (
+            <TouchableOpacity
+               style={{ backgroundColor: '#29434e' , padding: 10}}
+               onPress={()=>handleLogout(navigation)}
+             >
+               <Text style={{ marginVertical:1, borderRadius: 25, color: 'rgba(255,255,255,0.8)', fontSize: 20,}}> Logout </Text>
+             </TouchableOpacity>  
+          )
+        }
+
+      }) }
+      
+      />
+      {/* <Stack.Screen name='Button' component={BackButton} /> */}
+      {/* <Stack.Screen name='Card' component={InfoCard} /> */}
+      {/* <Stack.Screen name='Progress' component={ProgressBar} /> */}
+      {/* <Stack.Screen name='Profile' component={ProfileThumb} /> */}
+
+
+
+
+
+      
 
       </Stack.Navigator>
 
@@ -34,3 +98,6 @@ const styles = StyleSheet.create({
     fontSize: 'bold'
   }
 });
+
+
+// a4b53f7bff2578f11e48382f30a7933c
