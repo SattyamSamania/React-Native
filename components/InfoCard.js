54759af20ec82459529bd07612bc4e9e423b4f65
Sattyam Samania <67833888+SattyamSamania@users.noreply.github.com>
2023-07-21 16:24:45 +0530

    Update InfoCard.js

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,10 +1,75 @@
-import React from 'react'
+/* components/InfoCard.js*/
+import React from 'react';
+import { Text, View, Image, StyleSheet, Dimensions } from 'react-native';
+import ProgressBar from './ProgressBar';
+const screen = Dimensions.get('window');
 
+/**
+ * Our InfoCard component takes two props, "movie" and "director" 
+ *from MovieScreen. "movie" prop is the object holding movie 
+ *details like which we will use to show poster, title, overview, 
+ *and vote_average.
+ */
 
-export default function InfoCard(){
-    return(
+const InfoCard = ({ movie, director }) => {
+  return (
+    <View style={styles.infoCard}>
+      <Image
+        source={{
+          uri: `http://image.tmdb.org/t/p/w780${movie?.poster_path}`,
+        }}
+        style={styles.poster}
+      />
+      <View style={styles.textInfo}>
+        <Text style={styles.title}>{movie.original_title}</Text>
+        <Text style={{ color: 'white', fontWeight: 'bold' }}>PLOT</Text>
+        <Text style={{ color: 'white', fontSize: 10 }}>
+          {movie.overview.length < 100
+            ? movie.overview
+            : movie.overview.substr(0, 100) + '...'}
+        </Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <ProgressBar vote_average={movie.vote_average} />
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>
+            {movie.vote_average}
+          </Text>
+        </View>
         <>
-        
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>DIRECTOR</Text>
+          <Text style={{ color: 'white', fontSize: 10 }}>{director?.name}</Text>
         </>
-    )
-}
+      </View>
+    </View>
+  );
+};
+
+export default InfoCard;
+
+const styles = StyleSheet.create({
+  infoCard: {
+    position: 'absolute',
+    bottom: 10,
+    left: 10,
+    right: 10,
+    top: 40,
+    paddingRight: 10,
+    backgroundColor: 'rgba(21,21,21,0.5)',
+    borderRadius: 10,
+    overflow: 'hidden',
+    flexDirection: 'row',
+  },
+  poster: {
+    width: screen.width * 0.3,
+  },
+  title: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  textInfo: {
+    left: 10,
+    right: 10,
+    flex: 1,
+    justifyContent: 'space-evenly',
+  },
+});
