f2525e56e4b72afb44633b83e8ddbbc08ee3026e
Sattyam Samania <67833888+SattyamSamania@users.noreply.github.com>
2023-07-21 16:25:09 +0530

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -1,9 +1,27 @@
-import React from 'react'
+/* components/Loading.js */
+import React, { useEffect, useState } from 'react';
+import { Text, View, ActivityIndicator } from 'react-native';
 
-export default function  Loading(){
-    return(
-        <>
-        
-        </>
-    )
-}
+/**
+ * This Component will show a circular loading 
+ * indicator in the center of the screen while the servises.js 
+ * functions are fetching the data from TMDB.
+*/
+
+const Loading = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'rgb(21,21,21)',
+      }}>
+      <ActivityIndicator size="small" color="#0000ff" />
+      <Text style={{ color: 'white', alignSelf: 'center' }}>
+        loading too long? setup api keys in config/const.js ...
+      </Text>
+    </View>
+  );
+};
+
+export default Loading;
