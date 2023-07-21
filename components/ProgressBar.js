8b96890013f77d0270027325152a3661a1c58b47
Sattyam Samania <67833888+SattyamSamania@users.noreply.github.com>
2023-07-21 16:26:06 +0530

    Update ProgressBar.js

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,11 +1,33 @@
-import React from 'react'
+/* components/ProgressBar.js*/
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+/**
+*Just a bit of CSS to show vote_average on a scale of 100.
+*/
+const ProgressBar = ({ vote_average }) => {
+  return (
+    <View style={styles.main}>
+      <View
+        style={[styles.child, { width: Math.abs(10 * vote_average) }]}>
+     </View>
+    </View>
+  );
+};
 
+export default ProgressBar;
 
-export default function ProgressBar(){
-    return(
-        <>
-        
-        
-        </>
-    )
-}
+const styles = StyleSheet.create({
+  main: {
+    width: 100,
+    height: 10,
+    backgroundColor: 'tomato',
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  child: {
+    position: 'absolute',
+    height: 10,
+    backgroundColor: 'lightgreen',
+    borderRadius: 5,
+  },
+});
